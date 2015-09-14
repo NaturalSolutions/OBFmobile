3c97c17a7a6632e60e685003678321dfe3223b27
cdcvidal <celine_vidal@natural-solutions.eu>
2015-09-14 12:21:53 +0200

    Push one observation with one picture from browser on the local server

--- www/app/base/observation/lyt-observation.js
+++ www/app/base/observation/lyt-observation.js
@@ -38,30 +38,45 @@ function(Marionette, i18n, ObsModel) {
 			this.observationModel.set(fieldName, newValue).save();
 		},
 
-
+		//TODO if fields are not update departement and mission don't exist
 		sendObs: function(e){
 			var self = this;
 			//TODO add User in title if exist
 			e.preventDefault();
+
+
+			//clear data photo
+			//var args = this.observationModel.get('photo');
+			var clearPhoto = function(args){
+				var photo = [];
+				args.forEach(function(item, key){
+					console.log(item);
+
+					photo[key] = item.external_id ;
+				});
+				return photo.join();
+			};
+
 			//data expected by the server
 			var data = {
 						'title': this.observationModel.get('mission') + '_' + this.date,
 						'date': this.date,
 						'departement': this.observationModel.get('departement'),
-						'mission_id' : this.observationModel.get('mission')
+						'mission_id': this.observationModel.get('mission'),
+						'photo': clearPhoto(this.observationModel.get('photo')),
 						};
-			// var virginModel = new ObsModel();
-			// virginModel.save(data,{ajaxSync: true})
-			// 	.done(function(response){
-			// 		self.observationModel.set({
-			// 			'external_id':response.data[0].id,
-			// 			'shared': 1
-			// 		}).save();
-			// 	})
-			// 	.fail(function(error){
-			// 		console.log(error);
-			// 	})
-			// 	;
+			var virginModel = new ObsModel();
+			virginModel.save(data,{ajaxSync: true})
+				.done(function(response){
+					self.observationModel.set({
+						'external_id':response.data[0].id,
+						'shared': 1
+					}).save();
+				})
+				.fail(function(error){
+					console.log(error);
+				})
+				;
 		},
 
 		onShow: function() {
