75302306b94985db105f67e4d32e626c504f26f2
Vincent BOURGEOIS <vincent_bourgeois@natural-solutions.eu>
2016-01-05 09:16:15 +0100

    Manage empty list

--- www/modules/observation/observation_list.view.js
+++ www/modules/observation/observation_list.view.js
@@ -1,9 +1,16 @@
 'use strict';
 var Marionette = require('backbone.marionette'),
-  ListItem = require('./observation_list_item.view');
+  ListItem = require('./observation_list_item.view'),
+  _ = require('lodash');
+
+var emptyView = Marionette.LayoutView.extend({
+  tagName: 'li',
+  template: _.template('<b>Pas d\'obs</b>')
+});
 
 module.exports = Marionette.CollectionView.extend({
   tagName: 'ul',
   className: 'list-unstyled clearfix',
-  childView: ListItem
+  childView: ListItem,
+  emptyView: emptyView
 });
