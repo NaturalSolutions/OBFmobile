557d7793528838f453261abdfbb105ff517f20b1
cdcvidal <celine_vidal@natural-solutions.eu>
2016-01-28 11:27:46 +0100

    Update url server local

--- www/modules/main/config.js
+++ www/modules/main/config.js
@@ -8,8 +8,8 @@ module.exports = {
 
   //serveur local
   //test mobile
-  /*coreUrl: 'http://192.168.0.17/DRUPAL/OBF_BACK/www',
-  	apiUrl: 'http://192.168.0.17/DRUPAL/OBF_BACK/www/api',*/
+  // coreUrl: 'http://192.168.0.16/DRUPAL/OBF_BACK/www',
+  // 	apiUrl: 'http://192.168.0.16/DRUPAL/OBF_BACK/www/obfmobileapi',
 
   /*coreUrl: 'http://192.168.1.67/DRUPAL/OBF_BACK/www',
   	apiUrl: 'http://192.168.1.67/DRUPAL/OBF_BACK/www/obfmobileapi',*/
