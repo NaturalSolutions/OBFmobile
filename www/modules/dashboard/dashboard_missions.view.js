4e1633c85dff1c2671ec882a20a9fa063b432c35
cdcvidal <celine_vidal@natural-solutions.eu>
2016-01-28 18:08:47 +0100

    Successful missions are added to dashboard

--- www/modules/dashboard/dashboard_missions.view.js
+++ www/modules/dashboard/dashboard_missions.view.js
@@ -17,16 +17,19 @@ var ClassDef = Marionette.LayoutView.extend({
   },
 
   regions: {
-    list: '.list-outer'
+    list: '.list-outer',
+    listSucceed: '.list-succeed'
   },
 
   initialize: function() {
     this.missions = User.getCurrent().getAcceptedMissions();
+    this.missionsSucceed = User.getCurrent().getCompletedMissions();
   },
 
   serializeData: function() {
     return {
-      missions: this.missions
+      missions: this.missions,
+      missionsSucceed : this.missionsSucceed
     };
   },
 
@@ -34,8 +37,13 @@ var ClassDef = Marionette.LayoutView.extend({
     var collectionView = new CollectionView({
       collection: new Backbone.Collection(this.missions)
     });
+    var missionSucceedCollectionView = new CollectionView({
+      collection: new Backbone.Collection(this.missionsSucceed)
+    });
 
     this.showChildView('list', collectionView);
+    this.showChildView('listSucceed', missionSucceedCollectionView);
+
   }
 });
 
